2218fba778870e684b80a88858786d686223c8cc
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-06-17 09:40:24 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from "react-router-dom";
+import './index.css'
+import router from './Routes/Routes.jsx';
+import AuthProvider from './AuthProvider/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <AuthProvider>
+       <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router}></RouterProvider>
+       </QueryClientProvider>
+     </AuthProvider>
+  </React.StrictMode>,
+)
