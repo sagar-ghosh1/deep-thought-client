455f87e06303dfe82c013de9275c6e7c0192bbb0
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-06-17 14:10:09 +0600

    all section area

---
+++ src/Component/Pages/AllSection.jsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import { FaArrowRight } from 'react-icons/fa';
+
+const AllSection = () => {
+    return (
+        <div>
+            <div className='bg-slate-100 py-14'>
+                <div>
+                    <h1 className='text-6xl font-medium text-center mb-14'> <span className='text-orange-500'>All</span> Section</h1>
+                </div>
+                <div className='flex items-center justify-center container mx-auto'>
+                    <div className='grid gap-6 grid-cole-1 lg:grid-cols-2'>
+
+                        <div className='rounded-xl shadow-xl bg-white' data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+                            <div className='p-5 flex flex-col'>
+                                <div className='rounded-xl overflow-hidden flex'>
+                                    <div className='w-[25%]'>
+                                        <img className='m-auto' src="https://rb.gy/16coe" alt="" />
+                                    </div>
+                                    <div className='w-[75%] ml-8'>
+                                        <h1 className='text-2xl font-medium mb-6 text-green-500'>The Path of Serenity</h1>
+                                        <p className='font-medium'>Deepen your mind-body connection and cultivate inner peace through the transformative practices of yoga and meditation.</p>
+                                        <div className='flex'>
+                                            <p className='mt-5 font-bold'>Read More</p>
+                                            <p className='mt-6 ml-3'><FaArrowRight></FaArrowRight></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='rounded-xl shadow-xl bg-white' data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+                            <div className='p-5 flex flex-col'>
+                                <div className='rounded-xl overflow-hidden flex'>
+                                    <div className='w-[25%]'>
+                                        <img className='m-auto' src="https://rb.gy/8iinm" alt="" />
+                                    </div>
+                                    <div className='w-[75%] ml-8'>
+                                        <h1 className='text-2xl font-medium mb-6 text-green-500'>Unleashing Inner Peace</h1>
+                                        <p className='font-medium'>Unlock the healing power of yoga and meditation, promoting physical and mental well-being, and fostering a sense of balance and serenity.</p>
+                                        <div className='flex'>
+                                            <p className='mt-5 font-bold'>Read More</p>
+                                            <p className='mt-6 ml-3'><FaArrowRight></FaArrowRight></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='rounded-xl shadow-xl bg-white' data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+                            <div className='p-5 flex flex-col'>
+                                <div className='rounded-xl overflow-hidden flex'>
+                                    <div className='w-[25%]'>
+                                        <img className='m-auto' src="https://rb.gy/y1hxk" alt="" />
+                                    </div>
+                                    <div className='w-[75%] ml-8'>
+                                        <h1 className='text-2xl font-medium mb-6 text-green-500'>Harmony of Body and Mind</h1>
+                                        <p className='font-medium'>Experience the synergy of yoga and meditation, nurturing your body, calming your mind, and cultivating a profound sense of inner harmony.</p>
+                                        <div className='flex'>
+                                            <p className='mt-5 font-bold'>Read More</p>
+                                            <p className='mt-6 ml-3'><FaArrowRight></FaArrowRight></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='rounded-xl shadow-xl bg-white' data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+                            <div className='p-5 flex flex-col'>
+                                <div className='rounded-xl overflow-hidden flex'>
+                                    <div className='w-[25%]'>
+                                        <img className='m-auto' src="https://rb.gy/5bxk0" alt="" />
+                                    </div>
+                                    <div className='w-[75%] ml-8'>
+                                        <h1 className='text-2xl font-medium mb-6 text-green-500'>Journey to Tranquility</h1>
+                                        <p className='font-medium'>Discover the transformative benefits of yoga and meditation, promoting relaxation, stress reduction, and enhanced overall well-being.</p>
+                                        <div className='flex'>
+                                            <p className='mt-5 font-bold'>Read More</p>
+                                            <p className='mt-6 ml-3'><FaArrowRight></FaArrowRight></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='rounded-xl shadow-xl bg-white' data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+                            <div className='p-5 flex flex-col'>
+                                <div className='rounded-xl overflow-hidden flex'>
+                                    <div className='w-[25%]'>
+                                        <img className='m-auto' src="https://rb.gy/97ruz" alt="" />
+                                    </div>
+                                    <div className='w-[75%] ml-8'>
+                                        <h1 className='text-2xl font-medium mb-6 text-green-500'>Awakening the Spirit Within</h1>
+                                        <p className='font-medium'>Embark on a journey of self-discovery and self-care through the holistic practices of yoga and meditation, nurturing body, mind, and spirit.</p>
+                                        <div className='flex'>
+                                            <p className='mt-5 font-bold'>Read More</p>
+                                            <p className='mt-6 ml-3'><FaArrowRight></FaArrowRight></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='rounded-xl shadow-xl bg-white' data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+                            <div className='p-5 flex flex-col'>
+                                <div className='rounded-xl overflow-hidden flex'>
+                                    <div className='w-[25%]'>
+                                        <img className='m-auto' src="https://rb.gy/anzk3" alt="" />
+                                    </div>
+                                    <div className='w-[75%] ml-8'>
+                                        <h1 className='text-2xl font-medium mb-6 text-green-500'>The Art of Mindfulness</h1>
+                                        <p className='font-medium'>Immerse yourself in the ancient traditions of yoga and meditation, as you explore the depths of your being, finding balance, clarity, and rejuvenation.</p>
+                                        <div className='flex'>
+                                            <p className='mt-5 font-bold'>Read More</p>
+                                            <p className='mt-6 ml-3'><FaArrowRight></FaArrowRight></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AllSection;
